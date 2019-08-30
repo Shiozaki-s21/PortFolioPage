4feaca5f224decd7c57edaa9c7345100ae7abbe3
Kazuya <shiozaki5621@gmail.com>
2019-08-30 09:17:23 -0700

    small fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ class Homepage extends React.Component {
                                 <h2>My Products</h2>
                             </header>
                             <div className="col-4">
-                                <span className="image fit"><img src={locatterImage} width={200} height={200} alt="Top Page" /></span>
+                                <span className="image fit"><img src={locatterImage} alt="Top Page" /></span>
                                 <h3> Locatter </h3>
                                 <p>This is the new style SNS!.</p>
                                 <ul className="actions">
