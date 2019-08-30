8de34f95bc47487b89070d64c1e79b6706f42f50
Kazuya <shiozaki5621@gmail.com>
2019-08-30 14:21:13 -0700

    fix small

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends React.Component {
                     {/*link icons*/}
                     <li><a href="https://www.facebook.com/profile.php?id=100027270064972"><img src={facebook} height={30} width={30}/></a></li>
                     <li><a href="https://github.com/Shiozaki-s21"><img src={gituhb} height={30} width={30} /></a></li>
-                    <li><a href="https://www.linkedin.com/in/takahashi-kazuya-520347164/"><img src={linkedin} height={20} width={20} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/takahashi-kazuya-520347164/"><img src={linkedin} height={30} width={30} /></a></li>
                     <li><a href="https://medium.com/@KazuYaTaka" ><img src={medium} height={30} width={30} /></a></li>
                 </ul>
                 <ul className="copyright">
