0a6f4b8175549b77d9d2ee923cc955ea7f4cff5f
Kazuya <shiozaki5621@gmail.com>
2019-08-30 13:58:42 -0700

    size fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,10 @@ class Footer extends React.Component {
             <section id="footer">
                 <ul className="icons">
                     {/*link icons*/}
-                    <li><a href="https://www.facebook.com/profile.php?id=100027270064972"><img src={facebook}/></a></li>
-                    <li><a href="https://github.com/Shiozaki-s21" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="https://www.linkedin.com/in/takahashi-kazuya-520347164/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                    <li><a href="https://medium.com/@KazuYaTaka" className="icon alt fa-medium"><span className="label">Medium</span></a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100027270064972"><img src={facebook} height={30} width={30}/></a></li>
+                    <li><a href="https://github.com/Shiozaki-s21" className="icon alt fa-github"><img src={gituhb} height={30} width={30} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/takahashi-kazuya-520347164/" className="icon alt fa-linkedin"><img src={linkedin} height={30} width={30} /></a></li>
+                    <li><a href="https://medium.com/@KazuYaTaka" className="icon alt fa-medium"><img src={medium} height={30} width={30} /></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; 2019 Kazuya Takahashi</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
