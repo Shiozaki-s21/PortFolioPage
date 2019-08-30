680854427b15f687c1cf3668afcb725c2039d6e2
Kazuya <shiozaki5621@gmail.com>
2019-08-30 13:26:50 -0700

    samll fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <section id="footer">
                 <ul className="icons">
                     {/*link icons*/}
-                    <li><a href="https://www.facebook.com/profile.php?id=100027270064972" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100027270064972" className="fa-facebook-official"><span className="label">Facebook</span></a></li>
                     <li><a href="https://github.com/Shiozaki-s21" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="https://www.linkedin.com/in/takahashi-kazuya-520347164/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
                     <li><a href="https://medium.com/@KazuYaTaka" className="icon alt fa-medium"><span className="label">Medium</span></a></li>
