386f81c294a2b195b0bb469a27cc00af8f62d2f3
Kazuya <shiozaki5621@gmail.com>
2019-08-30 13:22:55 -0700

    fix small

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,10 +58,11 @@ class Homepage extends React.Component {
                         <div className="col-6">
                             <header className="major">
                                 <h2>My Skills</h2>
+                            </header>
                                 <p> - I'm good at Java + Spring FW.</p>
                                 <p> - iOS is better than Android. </p>
                                 <p> - In mobile development, I'm good at GPS things and Firebase</p>
-                            </header>
+
                             {/*TODO:Write description about my own skills */}
                             {/*<p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>*/}
                             {/*<p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>*/}
