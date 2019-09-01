1565f46c4c33b4371483b298595223c32bea5759
Kazuya <shiozaki5621@gmail.com>
2019-08-31 23:50:56 -0700

    Updates

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "commons.3b11fb6872bd136f66ba.css"
   },
   {
-    "url": "commons-208120eca0110a873a6d.js"
+    "url": "commons-caa206de7d5877ddd8ae.js"
   },
   {
     "url": "app-4637112463aa5aa65c2e.js"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "347658b5dec3d50b606a9129af00023f"
+    "revision": "b62de7dba5f8267f7a007013776f68f7"
   },
   {
     "url": "manifest.webmanifest",
